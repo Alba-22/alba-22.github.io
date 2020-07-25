15577d02b1801590506f061eb09217fd76e1f21e
Gustavo Alba <alba__22@outlook.com>
2020-07-25 02:44:22 -0300

    added 2 projects

--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -10,8 +10,8 @@ const RESOURCES = {
 "icons/Icon-192.png": "ac9a721a12bbc803b44f645561ecb1e1",
 "icons/Icon-512.png": "96e752610906ba2a93c65f8abe1645f1",
 "assets/packages/cupertino_icons/assets/CupertinoIcons.ttf": "115e937bb829a890521f72d2e664b632",
-"assets/AssetManifest.json": "c57bc9b5002f3b20ac81ba4452b2b71c",
-"assets/FontManifest.json": "3889ca932736f12e3d21615c2eda7bc3",
+"assets/AssetManifest.json": "d8d8242d8453b834bca02515887089eb",
+"assets/FontManifest.json": "7587c102a16672bc2bb42e4a87149fa6",
 "assets/assets/images/google-play-badge.png": "468ef7f576ce6cfef78231938349ab0b",
 "assets/assets/images/bitsblockchain.jpg": "9e22d095c4026af6cca5c1a8a78009e7",
 "assets/assets/images/cooprata/cooprata4.png": "88d020316ccd7b37a4dbddf3f8c54dc7",
@@ -22,17 +22,25 @@ const RESOURCES = {
 "assets/assets/images/bitsblockchain/bitsblockchain3.png": "a397330ea639c22537992cbf97ef2d4f",
 "assets/assets/images/bitsblockchain/bitsblockchain2.png": "17de6c39ba5a319fe6a424b0ca438652",
 "assets/assets/images/bitsblockchain/bitsblockchain4.png": "dbc006acd1061d61bde496071dabcc7d",
+"assets/assets/images/zenaide/zenaide1.png": "869b5dd0c7e2dedd656adc9e60f27211",
+"assets/assets/images/zenaide/zenaide2.png": "d0ed24fdc553a0af3c0db46c2502c13b",
+"assets/assets/images/zenaide/zenaide3.png": "88efa8a5e9e4b036c5f2cb389d66053c",
 "assets/assets/images/twitter.png": "247d003f20654c85d3aaba07811e36ec",
+"assets/assets/images/github-logo.png": "064ace4777ac86d7627f455f36975452",
 "assets/assets/images/linkedin.png": "30c453b7f5fbdb09ea0cb42a5dc7a6e5",
+"assets/assets/images/quarentify.jpeg": "78af19e0e99c3a1da97a38d8d60b0973",
 "assets/assets/images/profile.jpeg": "b89c607a2c81091c3c8ab5b6f82b7fbd",
 "assets/assets/images/github.png": "472739dfb5857b1f659f4c4c6b4568d0",
+"assets/assets/images/quarentify/quarentify1.png": "d54492d1c4fd5311c34bb83bb6c5b997",
+"assets/assets/images/quarentify/quarentify3.png": "ca706da49939fd301ead36b8848fa251",
+"assets/assets/images/quarentify/quarentify2.png": "a16ad94cb0b289880e7b92bc3d19071e",
 "assets/assets/images/cooprata.jpeg": "3091d0659a61bd6fb14a09107fa1bb7a",
 "assets/assets/images/zenaide.jpeg": "6f84c634250c9dc57cdf598294ec0ad0",
 "assets/assets/fonts/FiraCode-Bold.ttf": "01f3d4803613ee9556769509a85dba50",
 "assets/assets/fonts/FiraCode-Regular.ttf": "d60b1090972c3e6230c555347da880db",
-"assets/NOTICES": "6e5aeb5ca720e6a2e298fe49508e8fb7",
-"assets/fonts/MaterialIcons-Regular.ttf": "56d3ffdef7a25659eab6a68a3fbfaf16",
-"main.dart.js": "f2d9bb0393e51da609ac89e27067540a"
+"assets/NOTICES": "646b8b06e7d06214005eddefacc4ab56",
+"assets/fonts/MaterialIcons-Regular.otf": "a68d2a28c526b3b070aefca4bac93d25",
+"main.dart.js": "60f7debf6e4231b121e55fcff0f2b0fd"
 };
 
 // The application shell files that are downloaded before a service worker can
@@ -123,7 +131,7 @@ self.addEventListener("fetch", (event) => {
   if (event.request.url == origin || event.request.url.startsWith(origin + '/#')) {
     key = '/';
   }
-  // If the URL is not the the RESOURCE list, skip the cache.
+  // If the URL is not the RESOURCE list, skip the cache.
   if (!RESOURCES[key]) {
     return event.respondWith(fetch(event.request));
   }
@@ -146,11 +154,11 @@ self.addEventListener("fetch", (event) => {
 self.addEventListener('message', (event) => {
   // SkipWaiting can be used to immediately activate a waiting service worker.
   // This will also require a page refresh triggered by the main worker.
-  if (event.data == 'skipWaiting') {
+  if (event.data === 'skipWaiting') {
     return self.skipWaiting();
   }
 
-  if (event.message = 'downloadOffline') {
+  if (event.message === 'downloadOffline') {
     downloadOffline();
   }
 });
